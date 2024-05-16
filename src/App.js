52151eeec1bdb089ea99dc128f87f354417e4714
Maddie <8parmida8@gmail.com>
2024-05-17 01:08:31 +0330

    price filter UI

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import Products from "./components/Products/Products.js";
 import Filter from "./components/Filter/Filter.js";
 import { PRODUCT_DATA } from "./ProductItems.js";
 import { useState } from "react";
+import SortedPrices from "./components/Filter/SortedPrices.js";
 
 const App = () => {
   const [selectedTab, setSelectedTab] = useState("همه");
   function selectedTabHandler(category) {
     setSelectedTab(category);
   }
+  const [selectedPrice, setSelectedPrice] = useState(null);
+  function selectedPriceHandler(price) {
+    setSelectedPrice(price);
+  }
   return (
     <>
+      <SortedPrices
+        selectedPriceHandler={selectedPriceHandler}
+        selectedPrice={selectedPrice}
+      />
       <Filter
         productItems={PRODUCT_DATA}
         selectedTabHandler={selectedTabHandler}
---
+++ src/components/Filter/SortedPrices.js
@@ -0,0 +1,23 @@
+import "./Filter.css";
+
+function SortedPrices(props) {
+  const prices = [100, 200, 400];
+  return (
+    <div className="filter">
+      <ul className="filter__list">
+        {prices.map((price, index) => (
+          <li key={index}>
+            <button
+              className={props.selectedPrice === price ? "active" : ""}
+              onClick={() => props.selectedPriceHandler(price)}
+            >
+              {price}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default SortedPrices;
